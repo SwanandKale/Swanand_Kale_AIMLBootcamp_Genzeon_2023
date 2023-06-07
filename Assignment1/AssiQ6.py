8d6db59399bfd9ace576494b5ae147eeeb87756c
Swanand Kale <swanand.kale@genzeon.com>
2023-06-07 13:05:35 +0530

    Q6 completed

---
+++ Assignment1/AssiQ6.py
@@ -0,0 +1,11 @@
+x = float(input("Enter Number :: "))
+
+decimal = int(x)
+binary = bin(int(x))
+hexa = hex(int(x))
+octa = oct(int(x))
+
+print("Decimal :: ",decimal)
+print("Binary :: ",binary)
+print("HexaDecimal :: ",hexa)
+print("OctaDecimal :: ",octa)
