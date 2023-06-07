dec242e34f641fdba253774c6d91ed969871cede
Swanand Kale <swanand.kale@genzeon.com>
2023-06-07 13:01:07 +0530

    Addignment Q18

--- Assignment1/AssiQ18.py
+++ Assignment1/AssiQ18.py
@@ -10,14 +10,28 @@ matrix2=[[10,11,12],
 
 #Iterate matrix
 
-print(matrix1)
-print(matrix2)
+for row in matrix1:
+    print(row)
+
+for row in matrix2:
+    print(row)
+
 result= [[0, 0, 0],
           [0, 0, 0],
           [0, 0, 0]]
+
+print("Addition of matrix")
 for i in range(len(matrix1)):
     for j in range(len(matrix1[0])):
         result[i][j]=matrix1[i][j]+matrix2[i][j]
+
+for row in result:
+    print(row)
+
+print("Subtraction of matrix")
+for i in range(len(matrix1)):
+    for j in range(len(matrix1[0])):
+        result[i][j]=matrix1[i][j]-matrix2[i][j]
 
 for row in result:
     print(row)
